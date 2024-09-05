1401f19cfe02278883f41fde16a1dca7cd688879
MD. Muhiminul Haque <apon2041@gmail.com>
2024-09-05 18:18:08 +0600

    Update MailArrived.php

--- app/Events/MailArrived.php
+++ app/Events/MailArrived.php
@@ -11,24 +11,31 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class MailArrived implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     protected MailList $mailList;
+    protected string $action;
     /**
      * Create a new event instance.
      */
-    public function __construct(MailList $mailList)
+    public function __construct(MailList $mailList, $action = 'assign')
     {
         //
         $this->mailList = $mailList;
+        $this->action = $action;
     }
 
     public function broadcastWith(): array
     {
-        return $this->mailList->toArray();
+        return [
+            'listItem' => $this->mailList->toArray(),
+            'adminprev' => $this->action == "assign" ? 'remove' : 'prepend',
+            'userPrev' => $this->action == "assign" ? 'prepend' : 'remove',
+        ];
     }
 
     /**
@@ -38,8 +45,22 @@ class MailArrived implements ShouldBroadcastNow
      */
     public function broadcastOn(): array
     {
-        return [
-            new PrivateChannel('mail.' . $this->mailList->user),
-        ];
+        $eventChannels = [];
+        if ($this->action === 'assign') {
+            $eventChannels[] =  new PrivateChannel('mail.' . $this->mailList->user . ".userPrev.new");
+        }
+
+        $adminUserIds = DB::table('users')
+            ->join('user_has_role', 'users.id', '=', 'user_has_role.user_id')
+            ->join('user_roles', 'user_has_role.role_id', '=', 'user_roles.id')
+            ->whereIn('user_roles.name', ['Admin', 'Super Admin'])
+            ->pluck('users.id');
+        foreach ($adminUserIds as $userId) {
+            $eventChannels[] = new PrivateChannel("mail.{$userId}.adminprev.unassigned");
+            $eventChannels[] = new PrivateChannel("mail.{$userId}.adminprev.new");
+        }
+
+
+        return $eventChannels;
     }
 }
