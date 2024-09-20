f20b62afa1061917085c8a602ae03c3b5806851f
MD. Muhiminul Haque <apon2041@gmail.com>
2024-09-20 16:46:12 +0600

    Update Option.php

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,12 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Option extends Model
 {
     use HasFactory;
-    protected $fillable=['key','val','user'];
+    protected $fillable = ['key', 'val', 'user'];
     public $timestamps = false;
 
-   
+
+    public static function set($name, $value, $global = false)
+    {
+        $userId = Auth::user()->id;
+        if ($global) {
+            $userId = 0;
+        }
+        return self::updateOrCreate(['key' => $name, 'user' => $userId], ['value' => $value]);
+    }
+
+    public static function get($name, $default = null, $global = false)
+    {
+        $userId = Auth::user()->id;
+        if ($global) {
+            $userId = 0;
+        }
+        $setting = self::where('key', $name)->where('user', $userId)->first();
+        return $setting ? $setting->value : $default;
+    }
 }
