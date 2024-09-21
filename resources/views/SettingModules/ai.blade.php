12485ddfb6c294152892c62731c44031aa414004
MD. Muhiminul Haque <apon2041@gmail.com>
2024-09-21 18:12:17 +0600

    Update ai.blade.php

--- resources/views/SettingModules/ai.blade.php
+++ resources/views/SettingModules/ai.blade.php
@@ -17,14 +17,17 @@
                         <div class="flex items-center">
                             <label for="select" class="mr-2  text-gray-700 dark:text-gray-300">Select an
                                 option:</label>
-                            <select id="aiProvider" name="settings[ai_provider]" id="select"
+                            <select id="aiProvider" name="optionGlobal[ai_provider]" id="select"
                                 class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950">
                                 <option class="dark:text-gray-300" value="freebox"
-                                    @if ($Settings::get('ai_provider', 'gemini') === 'freebox') selected @endif>Open AI
+                                    @if ($Settings::get('ai_provider', 'gemini', true) === 'freebox') selected @endif>Open AI
                                     (Freebox)
                                 </option>
                                 <option class="dark:text-gray-300" value="gemini"
-                                    @if ($Settings::get('ai_provider', 'gemini') === 'gemini') selected @endif>Gemini
+                                    @if ($Settings::get('ai_provider', 'gemini', true) === 'gemini') selected @endif>Gemini
+                                </option>
+                                <option class="dark:text-gray-300" value="openai"
+                                    @if ($Settings::get('ai_provider', 'gemini', true) === 'openai') selected @endif>OpenAi
                                 </option>
                             </select>
                         </div>
@@ -39,11 +42,34 @@
                                     <div class="flex-1 md:ml-4 ml-0">
                                         <input
                                             class="w-full border rounded px-2 py-1  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
-                                            type="text" name="settings[ai_api_key]" value="<?php echo $Settings::get('ai_api_key', ''); ?>">
-                                        <span class="text-gray-500 text-sm">Key for api service provider</span>
+                                            type="text" name="optionGlobal[ai_api_key]" value="<?php echo $Settings::get('ai_api_key', '', true); ?>">
+                                        <span class="text-gray-500 text-sm">Api Key for Google - Gemini </span>
                                     </div>
                                 </div>
-
+                                <div
+                                    class="openai-settings optionField flex flex-col md:flex-row md:items-center justify-start mb-4">
+                                    <label class="w-32  text-gray-700 dark:text-gray-300">API KEY</label>
+                                    <!-- Adjust the width as needed -->
+                                    <div class="flex-1 md:ml-4 ml-0">
+                                        <input
+                                            class="w-full border rounded px-2 py-1  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
+                                            type="text" name="optionGlobal[ai_api_key_openai]"
+                                            value="<?php echo $Settings::get('ai_api_key_openai', '', true); ?>">
+                                        <span class="text-gray-500 text-sm">Api Key for OpenAI</span>
+                                    </div>
+                                </div>
+                                <div
+                                    class="openai-settings optionField flex flex-col md:flex-row md:items-center justify-start mb-4">
+                                    <label class="w-32  text-gray-700 dark:text-gray-300">Model</label>
+                                    <!-- Adjust the width as needed -->
+                                    <div class="flex-1 md:ml-4 ml-0">
+                                        <input
+                                            class="w-full border rounded px-2 py-1  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
+                                            type="text" name="optionGlobal[ai_model_openai]"
+                                            value="<?php echo $Settings::get('ai_model_openai', 'gpt-3.5-turbo', true); ?>">
+                                        <span class="text-gray-500 text-sm">AI model</span>
+                                    </div>
+                                </div>
                                 <div
                                     class="gemini-settings optionField flex flex-col md:flex-row md:items-center justify-start mb-4">
                                     <label class="w-32  text-gray-700 dark:text-gray-300">Data Model</label>
@@ -51,7 +77,8 @@
                                     <div class="flex-1 md:ml-4 ml-0">
                                         <input
                                             class="w-full border rounded px-2 py-1  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
-                                            type="text" name="settings[ai_data_model]" value="<?php echo $Settings::get('ai_data_model', 'gemini-pro'); ?>">
+                                            type="text" name="optionGlobal[ai_data_model]"
+                                            value="<?php echo $Settings::get('ai_data_model', 'gemini-pro', true); ?>">
                                         <span class="text-gray-500 text-sm">Data model of service provider</span>
                                     </div>
                                 </div>
@@ -61,14 +88,18 @@
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
                                         @php
-                                            $selectedModel = $Settings::get('ai_freebox_model', 'ai-content-generator'); // Assuming $Settings::get() retrieves the selected language
+                                            $selectedModel = $Settings::get(
+                                                'ai_freebox_model',
+                                                'ai-content-generator',
+                                                true,
+                                            ); // Assuming $Settings::get() retrieves the selected language
 
                                             $models = [
                                                 'ai-content-generator' => 'Content Generator',
                                                 'ai-email-generator' => 'Email Generator',
                                             ];
                                         @endphp
-                                        <select name="settings[ai_freebox_model]"
+                                        <select name="optionGlobal[ai_freebox_model]"
                                             class="block w-full p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950">
                                             @foreach ($models as $k => $name)
                                                 <option class="dark:text-gray-300" value="{{ $k }}"
@@ -85,7 +116,7 @@
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
                                         @php
-                                            $selectedLanguage = $Settings::get('ai_lang', ''); // Assuming $Settings::get() retrieves the selected language
+                                            $selectedLanguage = $Settings::get('ai_lang', '', true); // Assuming $Settings::get() retrieves the selected language
                                             $languages = [
                                                 'English',
                                                 'Bulgarian',
@@ -117,7 +148,7 @@
                                             ];
                                         @endphp
 
-                                        <select name="settings[ai_lang]"
+                                        <select name="optionGlobal[ai_lang]"
                                             class="block w-full p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950">
                                             @foreach ($languages as $language)
                                                 <option class="dark:text-gray-300" value="{{ $language }}"
@@ -132,12 +163,12 @@
                                     <label class="w-32  text-gray-700 dark:text-gray-300">Tone</label>
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
-                                        <select name="settings[ai_tone]"
+                                        <select name="optionGlobal[ai_tone]"
                                             class="block w-full p-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950">
                                             <option class="dark:text-gray-300" value="" disabled>Select an option
                                             </option>
                                             @php
-                                                $selectedOption = $Settings::get('ai_tone', 'Formal'); // Assume $selectedOption contains the value of the selected option
+                                                $selectedOption = $Settings::get('ai_tone', 'Formal', true); // Assume $selectedOption contains the value of the selected option
                                                 $options = [
                                                     'Formal' => 'Formal',
                                                     'Professional' => 'Professional',
@@ -161,25 +192,26 @@
                                     </div>
                                 </div>
                                 <div
-                                    class="gemini-settings optionField flex flex-col md:flex-row md:items-center justify-start mb-4">
+                                    class="gemini-settings openai-settings optionField flex flex-col md:flex-row md:items-center justify-start mb-4">
                                     <label class="w-32  text-gray-700 dark:text-gray-300">Creativity</label>
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
                                         <div class="flex">
-                                            <input title="Temperature" name="settings[ai_temperature]" id="temparature"
-                                                type="range" min="0" max="1"
-                                                value="{{ $Settings::get('ai_temperature', '0.7') }}" step="0.1"
+                                            @php
+                                                $creativity = $Settings::get('ai_temperature', '0.7', true);
+                                            @endphp
+                                            <input title="Temperature" name="optionGlobal[ai_temperature]"
+                                                id="temparature" type="range" min="0" max="1"
+                                                value=" {{ $creativity }}" step="0.1"
                                                 class="mt-2 range-input appearance-none w-10/12 bg-gray-400 rounded h-1 transition-all ease-in-out duration-300  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
                                                 oninput="document.getElementById('temparatureVal').textContent = this.value">
 
                                             <span id="temparatureVal" class="text-sm ml-2">
-                                                {{ $Settings::get('ai_temperature', '0.7') }}
+                                                {{ $creativity }}
                                             </span>
                                         </div>
 
-                                        {{-- <input class="w-full border rounded px-2 py-1" type="text"
-                                        name="settings[ai_temperature]" value="<?php echo $Settings::get('ai_temperature', '0.7'); ?>">
-                                     --}}
+                                        {{-- @dd($Settings::get('ai_temperature', '0.7')); --}}
                                         <span class="text-gray-500 text-sm">Controls the randomness of the output. Must
                                             be positive. Typical values are in the range: [0.0,1.0]</span>
                                     </div>
@@ -188,9 +220,9 @@
                                     <label class="w-32  text-gray-700 dark:text-gray-300">Prompt Prefix</label>
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
-                                        <textarea rows="2" name="settings[ai_prompt_prefix]"
+                                        <textarea rows="2" name="optionGlobal[ai_prompt_prefix]"
                                             class="p-2 rounded border border-gray-300 bg-transparent w-full h-full  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
-                                            placeholder="Write a reply in short-sentence to this email using the hints below:"><?php echo $Settings::get('ai_prompt_prefix', ''); ?></textarea>
+                                            placeholder="Write a reply in short-sentence to this email using the hints below:"><?php echo $Settings::get('ai_prompt_prefix', '', true); ?></textarea>
 
                                         <span class="text-gray-500 text-sm">Prefix text of prompt</span>
                                     </div>
@@ -199,9 +231,9 @@
                                     <label class="w-32  text-gray-700 dark:text-gray-300">Signature Filter</label>
                                     <!-- Adjust the width as needed -->
                                     <div class="flex-1 md:ml-4 ml-0">
-                                        <textarea placeholder="Best regards," rows="2" name="settings[ai_signeture_prefix]"
+                                        <textarea placeholder="Best regards," rows="2" name="optionGlobal[ai_signeture_prefix]"
                                             class="p-2 scrollbar-thin rounded border border-gray-300 bg-transparent w-full h-full  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
-                                            placeholder="Write a reply in short-sentence to this email using the hints below:"><?php echo $Settings::get('ai_signeture_prefix', ''); ?></textarea>
+                                            placeholder="Write a reply in short-sentence to this email using the hints below:"><?php echo $Settings::get('ai_signeture_prefix', '', true); ?></textarea>
 
                                         <span class="text-gray-500 text-sm">(each should new Line) Prefix text of
                                             signature and remove rest..</span>
@@ -215,29 +247,19 @@
                                             class=" block  text-gray-700 font-weight-bold dark:text-gray-300">Information
                                             About your Company </label>
                                         @php
-                                            $aboutField = $Settings::get('ai_about', 'ai_about_company');
+                                            $aboutField = $Settings::get('ai_about', 'ai_about_company', true);
                                         @endphp
-                                        <select id="aboutSwitch" name="settings[ai_about]"
-                                            class="ml-4 px-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950">
-                                            <option {{ $aboutField == 'ai_about_company' ? 'selected' : '' }}
-                                                class="dark:text-gray-300" value="ai_about_company">Default
-                                            </option>
-                                            <option {{ $aboutField == 'ai_about_company_faq' ? 'selected' : '' }}
-                                                class="dark:text-gray-300" value="ai_about_company_faq">Faq
-                                            </option>
-                                            <option {{ $aboutField == 'ai_about_company_new' ? 'selected' : '' }}
-                                                class="dark:text-gray-300" value="ai_about_company_new">New Info
-                                            </option>
-                                        </select>
-                                        <label id="waitMsg" class="hidden ml-2 text-red-400">Please Wait...</label>
                                     </div>
-                                    <textarea id="aboutData" rows="12" name="settings[{{ $aboutField }}]"
+                                    <textarea id="aboutData" rows="15" name="optionGlobal[{{ $aboutField }}]"
                                         class="scrollbar-thin p-2 rounded border border-gray-300 bg-transparent w-full h-full  dark:bg-gray-800 dark:text-gray-300 dark:border-gray-950"
-                                        placeholder="About Your Company"><?php echo $Settings::get($aboutField, ''); ?></textarea>
+                                        placeholder="About Your Company"><?php echo $Settings::get($aboutField, '', true); ?></textarea>
                                 </div>
                             </div>
-                        </div>
 
+                        </div>
+                        <div class="data-table-footer settings">
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -245,7 +267,28 @@
     </div>
 
     <x-slot name="script">
-        <script type="module"></script>
+        <script type="module">
+            document.addEventListener("DOMContentLoaded", function() {
+                $("#settingsForm").on('submit', function(e) {
+                    e.preventDefault();
+                    let route = $(this).attr('action');
+                    let settings = $(this).serialize();
+                    let target = $(e.target);
+                    target.find("button[type='submit']").html('<div class="load load03"><div></div></div>');
+                    axios.post(route, {
+                        settings
+                    }).then((response) => {
+                        target.find("button[type='submit']").html("Update");
+                        //console.log(response);
+                        if (response.data == 1) {
+                            ntf('Settings Updated', 'success');
+                        }
+                    }).catch((error) => {
+                        ntf(error, 'error');
+                    });
+                });
+            });
+        </script>
     </x-slot>
 
 
