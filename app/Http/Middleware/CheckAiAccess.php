2195faa8220b74ae29907bc900a6b6b5f3161c3b
MD. Muhiminul Haque <apon2041@gmail.com>
2024-09-23 18:04:19 +0600

    Create CheckAiAccess.php

---
+++ app/Http/Middleware/CheckAiAccess.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAiAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the user is authenticated and has admin privileges
+
+        if (Auth::user()->isAdmin()) {
+            return $next($request);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+}
