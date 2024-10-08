702b4caff5e619fab1690cda7a75ad6760d2bf56
MD. Muhiminul Haque <apon2041@gmail.com>
2024-10-08 18:11:33 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? $attributes->get('title', 'Default Title') }}</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    @php
+        $aiSettings = \App\Http\Controllers\AiService::getAiSettings();
+    @endphp
     <script>
         const APP_URL = "{{ config('app.url') }}";
-        const ATTACH = "/storage/{{ config('attachment.attachment_path') }}/";
-        const IN_ATTACH = "/storage/{{ config('attachment.inline_attachment_path') }}/";
+        const ATTACH = "/{{ config('attachment.attachment_path') }}/";
+        const IN_ATTACH = "/{{ config('attachment.inline_attachment_path') }}/";
         const USER_ID = {{ Auth::user()->id }};
         const USER_MAIL_CHANNEL = "{{ Auth::user()->getChannelID() }}";
+        const AI_OPTIONS = {
+            ai_provider: "{{ $aiSettings['provider'] }}",
+            ai_temperature: "{{ $aiSettings['creativity'] }}",
+            ai_tone: "{{ $aiSettings['tone'] }}"
+        };
     </script>
 </head>
 <!--detailsOpend-->
